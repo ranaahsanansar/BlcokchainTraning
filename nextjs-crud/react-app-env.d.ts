9db8381006ac62f4d550b20cdd8e9c42b067716d
Rana Ahsan Ansar <asn.cs21@gmail.com>
2023-07-25 11:14:03 +0500

    Uploading Projects

--- nextjs-crud/react-app-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react-scripts" />
-interface Window {
-    ethereum?: {
-      // value that is populated and returns true by the Coinbase Wallet mobile dapp browser
-      isCoinbaseWallet?: true
-      isMetaMask?: true
-      autoRefreshOnNetworkChange?: boolean
-    }
-  }
